9c1d64fb5b5e4f413ee4cb09282e3cdae756241a
HAEGUN LEE <106507190+Supreme-ignorance@users.noreply.github.com>
2022-09-21 02:25:15 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 44336 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> deck = new LinkedList<Integer>();
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			deck.offer(i);
+		}
+		
+		while (!deck.isEmpty()) {
+			if (deck.size() == 1) {
+				System.out.println(deck.poll());
+				break;
+			}
+			deck.poll();
+			deck.offer(deck.poll());
+		}
+	}
+}
