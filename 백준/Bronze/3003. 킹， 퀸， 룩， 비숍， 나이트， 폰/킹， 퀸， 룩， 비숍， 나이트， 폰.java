b3306921304ce6523387f19c706f6aeea68dfbf4
HAEGUN LEE <106507190+Supreme-ignorance@users.noreply.github.com>
2022-10-05 19:20:30 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int K = 1 - sc.nextInt();
+		int Q = 1 - sc.nextInt();
+		int R = 2 - sc.nextInt();
+		int B = 2 - sc.nextInt();
+		int N = 2 - sc.nextInt();
+		int P = 8 - sc.nextInt();
+
+		System.out.println(K + " " + Q + " " + R + " " + B + " " + N + " " + P);
+	}
+}
