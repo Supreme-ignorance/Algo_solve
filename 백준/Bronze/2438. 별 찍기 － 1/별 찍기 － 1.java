2007a3532e6e02b3219836662f0438bd519fbdbf
HAEGUN LEE <106507190+Supreme-ignorance@users.noreply.github.com>
2022-10-23 23:53:50 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 112 ms, Memory: 12948 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
