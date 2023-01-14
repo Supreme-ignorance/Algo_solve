a4d897d8c42c17e51b359e2068e4502cc2fe2931
HAEGUN LEE <106507190+Supreme-ignorance@users.noreply.github.com>
2023-01-15 00:06:06 +0900

    [Gold III] Title: 우주 탐사선, Time: 248 ms, Memory: 42824 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.java
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.java
@@ -34,7 +34,8 @@ public class Main {
                 map[r][c] = Integer.parseInt(st.nextToken());
             }
         }
-
+        
+        // 중복 가능이니까 플루이드 워셜이야~
         for(int k = 0; k < N; k++){
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < N; j++){
